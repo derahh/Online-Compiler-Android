e72fec33891b253f71939f12304b78173f09424a
Dedi Rahmansah <derah666@gmail.com>
2020-02-04 14:27:30 +0700

    Create APIService.java

---
+++ app/src/main/java/com/derahh/onlinecompiler/APIService.java
@@ -0,0 +1,12 @@
+package com.derahh.onlinecompiler;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+
+    @POST("execute")
+    Call<String> execute(@Body PostData postData);
+}
